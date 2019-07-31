48c0ff72ff4e7d03910774eff397f8fe99c861b3
ShiningPeng <1494228462@qq.com>
2019-07-31 12:08:33 +0800

    'update'

--- spider/index.js
+++ spider/index.js
@@ -105,58 +105,8 @@ const getOneData = function () {
 //       })
 //   })
 // };
-const getArticle = function () {
-  return new Promise((resolve, reject) => {
-    superagent.get('http://wufazhuce.com/article/3928').end((err, res) => {
-      if (err) {
-        reject(err);
-      }
-      let $ = cheerio.load(res.text);
-      let oneArticle = $('#main-container .one-articulo');
-      //获取开头句子
-      let startSentence = oneArticle.find('.comilla-cerrar').text().trim();
-      // console.log('startSentence',startSentence);
-      //获取文章标题
-      let articleTitle = oneArticle.find('.articulo-titulo').text().trim();
-      //获取文章作者
-      let articleAuthor = oneArticle.find('.articulo-autor').text().trim();
-      //获取文章的内容
-      let articleContent = oneArticle.find('.articulo-contenido').text().trim();
-      // console.log(articleContent);
-      let article = {
-        startSentence, articleTitle, articleAuthor, articleContent
-      }
-      // let articleCon = [];
-      // articleContent.each((e) => {
-      //   // console.log(e,pNode);
-      //   articleCon.push({
-      //     content:$(articleContent[e]).find('p').text()
-      //   })
-      // })
-      resolve(article);
-      console.log('article:---',article);
-    })
-  })
-}
-let getOneQuestion = function(){
-  return new Promise((resolve, reject) => {
-    superagent.get('http://wafazhuce.com/question/2552').end((err, res) => {
-      if(err){
-        reject(err);
-      }
-      let $ = cheerio.load(res);
-      console.log(res);
-      // let question = $('#main-container .one-cuestion');
-      // let oneQuestion = {questionTitle:question.find('.cuestion-q-icono').text().trim(), questionContent:question.find('.cuestion-contenido').text().trim()}
-      let oneQuestion=[];
-      resolve(oneQuestion);
-      // console.log('oneQuestion',oneQuestion);
-    })
-  })
-}
+
 
 // getTodayArticleTitle();
-// getWeatherTpis();
+getWeatherTpis();
 // getOneData();
-// getArticle();
-getOneQuestion();
