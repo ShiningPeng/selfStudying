fabf96f727fa206d2a950f3c93be3374044294b4
ShiningPeng <1494228462@qq.com>
2019-08-31 22:36:23 +0800

    '正则'

---
+++ testRegExp.js
@@ -0,0 +1,18 @@
+var str = '14klsj4_75 ji';
+
+// console.log(str.match(/\D/));
+//[ 'k', index: 2, input: '14klsj475', groups: undefined ]
+
+// console.log(str.match(/\D/g));
+// console.log(str.match(/\d/g));
+// console.log(str.match(/[0-9]/g));
+// console.log(str.match(/[^0-9]/g));
+// [ 'k', 'l', 's', 'j' ]
+// [ '1', '4', '4', '7', '5' ]
+// [ '1', '4', '4', '7', '5' ]
+// [ 'k', 'l', 's', 'j' ]
+
+// console.log(str.match(/\w/));
+// console.log(str.match(/\w/g));
+// [ '1', index: 0, input: '14klsj4_75 ji', groups: undefined ]
+// [ '1', '4', 'k', 'l', 's', 'j', '4', '_', '7', '5', 'j', 'i' ]
