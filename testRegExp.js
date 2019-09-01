f18629811d229ba2c24c5befa84516a73cdf1898
ShiningPeng <1494228462@qq.com>
2019-09-01 23:33:31 +0800

    'update'

--- testRegExp.js
+++ testRegExp.js
@@ -13,6 +13,20 @@ var str = '14klsj4_75 ji';
 // [ 'k', 'l', 's', 'j' ]
 
 // console.log(str.match(/\w/));
-// console.log(str.match(/\w/g));
+// console.log(str.match(/\w/g)); 
 // [ '1', index: 0, input: '14klsj4_75 ji', groups: undefined ]
 // [ '1', '4', 'k', 'l', 's', 'j', '4', '_', '7', '5', 'j', 'i' ]
+
+// console.log(str.match(/\w/g));
+// console.log(str.match(/\W/g));
+// [ '1', '4', 'k', 'l', 's', 'j', '4', '_', '7', '5', 'j', 'i' ]
+// [ ' ' ]
+
+// console.log(str.match(/\s/g));
+// console.log(str.match(/\S/g));
+// [ ' ' ]
+// [ '1', '4', 'k', 'l', 's', 'j', '4', '_', '7', '5', 'j', 'i' ]
+
+// testQQRegExp
+// console.log(/^[1-9][0-9]{5,10}/.test('4812354785464647'));
+// console.log(/\d{6,10}/.test('163643544244'))
