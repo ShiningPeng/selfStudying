036322d86bdfc7b01c5f804862df81042ef0b8cc
ShiningPeng <1494228462@qq.com>
2019-09-10 23:06:31 +0800

    '学校HTML5课程'

---
+++ 学校H5课程/期末大作业/houtaiguanli/src/pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="input-username-box">
+      用户名：<el-input v-model="userName" placeholder="请输入内容"></el-input>
+    </div>
+      <div class="input-password-box">
+        密码：<el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+      </div>
+    <div class="login-btn">
+      <el-button round>登录</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Login',
+  data () {
+    return {
+      userName:'',
+      password:''
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
