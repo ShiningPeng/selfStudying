34f2043c8fb5b06dade63f4ea18bff4b6757f50c
ShiningPeng <1494228462@qq.com>
2019-09-25 23:44:01 +0800

    'update'

--- 学校H5课程/期末大作业/houtaiguanli/src/main.js
+++ 学校H5课程/期末大作业/houtaiguanli/src/main.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Input, Button, Menu, MenuItem, Submenu, RadioGroup, RadioButton, Form, FormItem } from 'element-ui'
+import {Input, Button, Menu, MenuItem, Submenu, RadioGroup, RadioButton, Form, FormItem, Table, TableColumn } from 'element-ui'
 
-Vue.use(Input).use(Button).use(Menu).use(MenuItem).use(Submenu).use(RadioGroup).use(RadioButton).use(Form).use(FormItem);
+Vue.use(Input).use(Button).use(Menu).use(MenuItem).use(Submenu).use(RadioGroup).use(RadioButton).use(Form).use(FormItem).use(Table).use(TableColumn);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
