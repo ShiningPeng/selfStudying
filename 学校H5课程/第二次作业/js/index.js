c481d8b6f83290c1ee67032cea200aab29ec945a
ShiningPeng <1494228462@qq.com>
2019-11-01 23:24:40 +0800

    'update'

--- 学校H5课程/第二次作业/js/index.js
+++ 学校H5课程/第二次作业/js/index.js
@@ -1,5 +1,35 @@
+//验证输入的邮件地址（用户名）是否合法
+function checkUserName(str){
+  var regular =  /^[a-zA-Z](\w){5, 17}$/;
+  var res = regular.test(str);
+  var result  = {regular, res};
+  return result;
+} 
+//判断密码是否合法
+function checkPassword(str){
+  var regular = /^(\S){6,16}$/; 
+  var res = regular.test(str);
+  var result  = {regular, res};
+  return result;
+}
+//判断两次输入的密码是否一致
+function checkIsSamePassword(firstPassword, secondPassword){
+  if(firstPassword === secondPassword){
+    return true;
+  }else{
+    return false;
+  }
+}
+//判断输入的验证码是否和右侧随机产生的验证码一致
+function checkIsSameVerificationCode(inputCode, rightCode){
+  if(inputCode === rightCode){
+    return true;
+  }else{
+    return false;
+  }
+}
 //产生随机验证码
-function random_str(len = 4 ){
+function random_str(len = 5 ){
   const $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
   const maxPos = $chars.length;
   let str = '';
@@ -8,3 +38,5 @@ function random_str(len = 4 ){
   }
   return str;
 }
+
+// console.log(random_str())
