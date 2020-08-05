c8da70e400e04cce03e5b454e5f96d62af3541e3
ShiningPeng <1494228462@qq.com>
2020-08-05 09:50:08 +0800

    update

--- symusic/src/application/Recommend/index.js
+++ symusic/src/application/Recommend/index.js
@@ -7,43 +7,59 @@ import { renderRoutes } from 'react-router-config';
 
 
 function Recommend(props) {
-    console.log(props);
-    
-    return (
-        // <div/>
-        // <Content play={songsCount}>
-        //     <Scroll className="list" onScroll={forceCheck}>
-        //         <div>
-        //             <Slider bannerList={bannerListJS}></Slider>
-        //             <RecommendList recommendList={recommendListJS}></RecommendList>
-        //         </div>
-        //     </Scroll>
-        //     {enterLoading ? <EnterLoading><Loading></Loading></EnterLoading> : null}
-        //     {renderRoutes(props.route.routes)}
-        // </Content>
-        <>Recommend</>
-    );
+  console.log(props);
+  const { bannerList, recommendList, songsCount, enterLoading } = props;
+
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+
+  useEffect(() => {
+    if (!bannerList.size) {
+      getBannerDataDispatch();
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch();
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() : [];
+
+  return (
+    // <div/>
+    // <Content play={songsCount}>
+    //     <Scroll className="list" onScroll={forceCheck}>
+    //         <div>
+    //             <Slider bannerList={bannerListJS}></Slider>
+    //             <RecommendList recommendList={recommendListJS}></RecommendList>
+    //         </div>
+    //     </Scroll>
+    //     {enterLoading ? <EnterLoading><Loading></Loading></EnterLoading> : null}
+    //     {renderRoutes(props.route.routes)}
+    // </Content>
+    <>Recommend</>
+  );
 }
 
 
 // 映射Redux全局的state到组件的props上
 const mapStateToProps = (state) => ({
-    bannerList: state.getIn(['recommend', 'bannerList']),
-    recommendList: state.getIn(['recommend', 'recommendList']),
-    // songsCount: state.getIn(['player', 'playList']).size,
-    enterLoading: state.getIn(['recommend', 'enterLoading'])
-  });
-  // 映射dispatch到props上
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      getBannerDataDispatch() {
-        dispatch(actionTypes.getBannerList());
-      },
-      getRecommendListDataDispatch() {
-        dispatch(actionTypes.getRecommendList());
-      },
-  
-    }
-  };
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+  // songsCount: state.getIn(['player', 'playList']).size,
+  enterLoading: state.getIn(['recommend', 'enterLoading'])
+});
+// 映射dispatch到props上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getBannerDataDispatch() {
+      dispatch(actionTypes.getBannerList());
+    },
+    getRecommendListDataDispatch() {
+      dispatch(actionTypes.getRecommendList());
+    },
+
+  }
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend));
